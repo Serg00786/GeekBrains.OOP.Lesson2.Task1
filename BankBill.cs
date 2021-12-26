f72a44bf1719e04c4c1552130ed4be1da47609e1
Serg <a_s_v86@mail.ru>
2021-12-26 22:43:26 +0300

    Finalyzed

--- BankBill.cs
+++ BankBill.cs
@@ -8,5 +8,42 @@ namespace GeekBrains.OOP.Lesson2.Task1
 {
     class BankBill
     {
+        private long BillNumber;
+        private double Balance;
+        internal enum BillTypes{
+             CompanyBill,
+             PrivateBill
+        }
+        private BillTypes TheBillTypes;
+
+        internal long GetBillNumber()
+        {
+            return BillNumber;
+        }
+
+        internal double GetBalance()
+        {
+            return Balance;
+        }
+
+        internal BillTypes GetBillTypes()
+        {
+            return TheBillTypes;
+        }
+
+        internal long SetBillNumber(long value)
+        {
+            return BillNumber = value;
+        }
+
+        internal double SetBalance(double value)
+        {
+            return Balance=value;
+        }
+
+        internal BillTypes SetBillTypes(BillTypes billTypes)
+        {
+            return TheBillTypes=BillTypes.CompanyBill;
+        }
     }
 }
